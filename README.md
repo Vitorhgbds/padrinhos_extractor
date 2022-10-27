28cba580d071a0c4842fdc79bdcc2ebc02ff1865
Garcez <vitor.hgbds@hp.com>
2022-10-27 12:30:42 -0300

    automatizando mensagens, removendo users que já são registrados

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # padrinhos_extractor
 
-após baixar o arquivo csv do forms e colocar na raiz da pasta do projeto basta executar o seguinte comando:
+1. Criar uma pasta com o nome de `files`
+2. Criar um arquivo na pasta `files` `padrinhos_registrados.csv` com a primeira linha no seguinte formato:
+`nome,curso`
+3. baixar o arquivo csv do forms de padrinhos e colocar na pasta `files`
+
+após baixar o arquivo csv do forms e colocar na pasta files do projeto basta executar o seguinte comando:
 ```console
 pip install -r requirements.txt
+
 ```
 Então, passe como argumento o nome do arquivo na hora de rodar o programa, utilizando a seguinte sintaxe:
 ```console
--- extract_padrinhos.py
+++ extract_padrinhos.py
@@ -11,22 +11,37 @@ for i,curso in enumerate(df['Qual o seu curso?']):
         curso_indexes[curso] = []
     curso_indexes[curso].append(i)
 
+padrinhos_registrados = pd.read_csv('files/padrinhos_registrados.csv')
+
 mensagens = []
 for curso in curso_indexes.keys():
     padrinhos = curso + " \n \n "
     for pessoas_indexes in curso_indexes[curso]:
-        padrinhos = padrinhos + 'Nome -> ' + df['Identifique seu nome'].get(pessoas_indexes) + ' \n '
-        padrinhos = padrinhos + 'Bicho favorito -> ' + df['Qual seu bicho favorito?'].get(pessoas_indexes) + ' \n '
-        padrinhos = padrinhos + 'Area de interesse -> ' + df['Qual sua area de interesse?'].get(pessoas_indexes) + ' \n '
-        padrinhos = padrinhos + 'Cor favorita -> ' + df['Cor favorita'].get(pessoas_indexes) + ' \n '
-        padrinhos = padrinhos + 'Hobbie -> ' + df['Um Hobbie'].get(pessoas_indexes) + ' \n '
-        padrinhos = padrinhos + 'Musica -> ' + df['Musica, banda ou genero musical favorito'].get(pessoas_indexes) + ' \n '
-        padrinhos = padrinhos + 'preferencia de contato -> ' + df['Como prefere ser contatado?'].get(pessoas_indexes) + ' \n '
-        padrinhos = padrinhos + 'telefone -> ' + str(df['Numero de telefone (opcional)'].get(pessoas_indexes)) + ' \n '
-        padrinhos = padrinhos + 'nick do disc -> ' + str(df['Nick do discord (opcional)'].get(pessoas_indexes)) + ' \n \n '
+        nome = df['Identifique seu nome'].get(pessoas_indexes).lower()
+        if (nome not in padrinhos_registrados['nome'].to_list()):
+            padrinhos_registrados.loc[len(padrinhos_registrados.index)] = [nome,curso]
+            padrinhos = padrinhos + 'Nome -> ' + nome + ' \n '
+            padrinhos = padrinhos + 'preferencia de contato -> ' + df['Como prefere ser contatado?'].get(pessoas_indexes) + ' \n '
+            padrinhos = padrinhos + 'telefone -> ' + str(df['Numero de telefone (opcional)'].get(pessoas_indexes)) + ' \n '
+            padrinhos = padrinhos + 'nick do disc -> @' + str(df['Nick do discord (opcional)'].get(pessoas_indexes)) + ' \n \n '
+            padrinhos = padrinhos + '```\n'
+            padrinhos = padrinhos + 'Bicho favorito -> ' + df['Qual seu bicho favorito?'].get(pessoas_indexes) + ' \n '
+            padrinhos = padrinhos + 'Area de interesse -> ' + df['Qual sua area de interesse?'].get(pessoas_indexes) + ' \n '
+            padrinhos = padrinhos + 'Cor favorita -> ' + df['Cor favorita'].get(pessoas_indexes) + ' \n '
+            padrinhos = padrinhos + 'Hobbie -> ' + df['Um Hobbie'].get(pessoas_indexes) + ' \n '
+            padrinhos = padrinhos + 'Musica -> ' + df['Musica, banda ou genero musical favorito'].get(pessoas_indexes) + ' \n '
+            padrinhos = padrinhos + '```\n'
+            padrinhos = padrinhos + '__________\n'
     mensagens.append(padrinhos)
 
-f = io.open("padrinhos.txt", "w", encoding='utf8')
+
+#f = io.open("alreadyRegistered.txt", "w", encoding='utf8')
+#for nome_padrinho in alreadyRegistered:    
+#    f.write(nome_padrinho)
+#f.close()
+padrinhos_registrados.to_csv('files/padrinhos_registrados.csv')
+
+f = io.open("files/padrinhos_para_registrar.txt", "w", encoding='utf8')
 for mensagem in mensagens:    
     f.write(mensagem)
 f.close()
